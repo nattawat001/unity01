7f8c23d92bc356ed5f9e5fc9a78ffc4ae8bb831b
nattawat001 <108708210+nattawat001@users.noreply.github.com>
2022-07-05 15:03:13 +0700

    unitySphere

--- Assets/Script/Gameplay.cs
+++ Assets/Script/Gameplay.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Gameplay : MonoBehaviour
 {
-
+ int g = 20,r = -20, b = 10 ,sum = 0;
+ public TextMeshProUGUI txtDisplay;
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +19,62 @@ public class Gameplay : MonoBehaviour
                 if(hit.collider.gameObject.name.CompareTo("RedSphere") == 0)
                 {
                     hit.collider.gameObject.SetActive(false);
+                    sum = sum + r;
                 }
+                if(hit.collider.gameObject.name.CompareTo("Red01") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + r;
+                }
+                if(hit.collider.gameObject.name.CompareTo("Red02") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + r;
+                }
+                if(hit.collider.gameObject.name.CompareTo("Red03") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + r;
+                }
+                 if(hit.collider.gameObject.name.CompareTo("BlueSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + b;
+                    
+                }
+                 if(hit.collider.gameObject.name.CompareTo("Blue01") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + b;
+                    
+                }
+                 if(hit.collider.gameObject.name.CompareTo("Blue02") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + b;
+                    
+                }
+
+                if(hit.collider.gameObject.name.CompareTo("GreenSphere") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + g;
+                    
+                }
+                if(hit.collider.gameObject.name.CompareTo("Green01") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + g;
+                    
+                }
+                if(hit.collider.gameObject.name.CompareTo("Green02") == 0)
+                {
+                    hit.collider.gameObject.SetActive(false);
+                    sum = sum + g;
+                    
+                }
+                Debug.Log(sum);
+                txtDisplay.text ="POINT" + sum;
             }
         }
     }
