6c1d67481b331c07312e039b0cbe4139509a10f8
nattawat001 <108708210+nattawat001@users.noreply.github.com>
2022-07-05 10:42:02 +0700

    print Hello world

---
+++ Assets/Myscript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Myscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+    }
+
+    
+}
